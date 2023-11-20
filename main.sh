0ff4bc3aa3fdf6b814c7ec23ad5338ce895f07fb
ccaballerof00 <48997787+ccaballerof00@users.noreply.github.com>
2023-11-19 22:40:17 -0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -5,9 +5,9 @@
 #SBATCH --nodelist=g001
 #SBATCH --mem=32GB
 module load gcc/9.2.0
-module load cuda/11.4
-module load python/3.9.2
-export PATH=/usr/local/cuda-11.4/targets/x86_64-linux/lib:$PATH
-export LD_LIBRARY_PATH=/usr/local/cuda-11.4/lib64:$LD_LIBRARY_PATH
+module load cuda/12.2
+module load python/3.9.18
+export PATH=/usr/local/cuda-12.2/targets/x86_64-linux/lib:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda-12.2/lib64:$LD_LIBRARY_PATH
 export PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:100
 python3 main.py \
